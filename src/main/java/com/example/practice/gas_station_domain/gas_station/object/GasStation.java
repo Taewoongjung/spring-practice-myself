37259651c7482c22c2fdd79caa8eee0ad2a8e280
Taewoongjung <aipooh8882@naver.com>
2022-06-30 19:57:30 +0900

    Add GasStation object

--- src/main/java/com/example/practice/gas_station_domain/gas_station/object/GasStation.java
+++ src/main/java/com/example/practice/gas_station_domain/gas_station/object/GasStation.java
@@ -6,6 +6,44 @@ public class GasStation {
     private String name; // 주유소 이름
     private String brand; // 주유소 브랜드
     private Integer price; // L 당 가격
+    private String ownerName; // 사장님 성함
 
+    public GasStation(Long memberId, String name, String brand, Integer price, String ownerName) {
+        this.memberId = memberId;
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.ownerName = ownerName;
+    }
 
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    @Override
+    public String toString() {
+        return "GasStation{" +
+                "memberId=" + memberId +
+                ", name='" + name + '\'' +
+                ", brand='" + brand + '\'' +
+                ", price=" + price +
+                ", ownerName='" + ownerName + '\'' +
+                '}';
+    }
 }
